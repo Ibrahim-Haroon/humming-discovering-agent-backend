f5f9f38f7bad69e87a04448e1cb0138f86a8d60b
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-11-24 10:00:19 -0500

    created generic LLM interface to be able to use any provider

---
+++ src/llm/service/llm_response_service.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class LlmResponseService(ABC):
+    @abstractmethod
+    def response(self, role: str, prompt: str) -> str:
+        """
+        This method is used to as a baseline behavior for all LLM Response Services
+
+        :param role: The behavior/persona for the model to inherit
+        :param prompt: The task for the model to complete
+        :return: response from LLM
+        :rtype: str
+        """
+        pass
