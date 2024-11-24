342570da40d29742a520772e4a422ea1c8854e72
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-11-24 10:55:05 -0500

    Fixed a typo in the comment for the response method in LlmResponseService.

--- src/llm/service/llm_response_service.py
+++ src/llm/service/llm_response_service.py
@@ -5,7 +5,7 @@ class LlmResponseService(ABC):
     @abstractmethod
     def response(self, role: str, prompt: str) -> str:
         """
-        This method is used to as a baseline behavior for all LLM Response Services
+        This method is used as a baseline behavior for all LLM Response Services
 
         :param role: The behavior/persona for the model to inherit
         :param prompt: The task for the model to complete
