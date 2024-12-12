d684be3bd6af2055a77108115cfb7f62ff8e208e
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-12-11 19:53:32 -0500

    v1 prompts for testing

--- src/llm/template/llm_prompt_contextualizer.py
+++ src/llm/template/llm_prompt_contextualizer.py
@@ -8,6 +8,27 @@ class LlmPromptContextualizer:
 
     @staticmethod
     def generate_initial_prompt(
-            business_type
+            business_type: str
     ):
         return LlmTemplate.initial_customer_prompt(business_type)
+
+    @staticmethod
+    def generate_response_prompt(
+            business_type: str,
+            response: str
+    ):
+        return LlmTemplate.generate_customer_prompt(
+            business_type,
+            response
+        )
+
+
+    @staticmethod
+    def generate_analysis_prompt(
+            business_type: str,
+            text: str
+    ):
+        return LlmTemplate.generate_analysis_prompt(
+            business_type,
+            text
+        )
--- src/llm/template/llm_template.py
+++ src/llm/template/llm_template.py
@@ -1,9 +1,16 @@
 from textwrap import dedent
 
-ROLE = dedent(
+CUSTOMER_ROLE = dedent(
     """
-    Your role is to simulate realistic customer behavior and responses to discover an AI system's conversational
-    capabilities. Never make up information.
+    You're an agent testing AI front-desk assistant services, you must instruct another AI with how to respond to the
+    AI front-desk assistant service.
+     
+    """
+).strip()
+
+ANALYSIS_ROLE = dedent(
+    """
+    You're an expert at analyzing customer service transcription calls
     """
 ).strip()
 
@@ -22,7 +29,7 @@ class LlmTemplate:
         ).strip()
 
     @staticmethod
-    def generate_customer_prompt(business_type: str):
+    def generate_customer_prompt(business_type: str, response: str):
         return dedent(
             f"""
             
@@ -32,7 +39,79 @@ class LlmTemplate:
             {business_type}
             </business_type>
             
-            TODO
+            <response>
+            {response}
+            </response>
             
+            You must go based on the business type, the response you should respond with, and how previous calls went.
+            Construct the AI on how to answer the question which the call ended on previously. 
+            
+            One thing you will notice for the call transcription is that it's a mixture of the front-desk assistant and
+            the AI talking simultaneously, which you're instructing on how to respond. You must discern between them 
+            both to understand what's happening
+            
+            For example:
+            previous call: hello thank you for calling turbo this is tom speaking are you an existing customer with us
+            end call thank you for calling turbo goodbye
+            
+            question you need to answer: are you an existing customer?
+            
+            good instructions: You are a customer talking to a front-desk assistant for a {business_type}. When asked
+            if you're an existing customer say yes, then just end the call after they ask the next question
+        
+            You *must* instruct with ending the call after the new question has been answered. Also just used the
+            format of `When asked {{X}}, respond with {{Y}} if it makes sense. Do not overcomplicate. 
+            """
+        ).strip()
+
+    @staticmethod
+    def generate_analysis_prompt(business_type: str, text: str):
+        return dedent(
+            f"""
+            You are provided the following information:
+
+            <business_type>
+            {business_type}
+            </business_type>
+            
+            <text>
+            {text}
+            </text>
+            
+            You must go based on the business type, text which is a transcription of the call, and how previous calls
+            went. You need to analyze whether the call went to completion, for example when the front-desk assistant
+            hangs up the call. This can be because it transferred to a human, cannot help, or completed everything
+            alone.
+            
+            One thing you will notice for the call transcription is that it's a mixture of the front-desk assistant and
+            the AI talking simultaneously, which you're instructing on how to respond. You must discern between them 
+            both to understand what's happening
+            
+            If it's not terminal then you need to determine all possible responses, it can be binary or also non-binary.
+            Here are examples of both:
+                Non-binary:
+                    hello thank you for calling turbo this is tom speaking are you a gold, silver, or bronze customer 
+                    with us end call thank you for calling turbo goodbye
+                    
+                    Response: False|Yes, I'm a gold customer;Yes, I'm a silver customer;Yes, I'm a bronze customer
+                Binary:
+                    hello thank you for calling turbo this is tom speaking is this an emergency end call thank you for
+                    calling turbo goodbye
+                    
+                    Response: False|Yes, this is an emergency; No, this is not an emergency
+            
+            Finally here is an example of terminal:
+            hello thank you for calling turbo this is tom speaking is this an emergency yes this is an emergency
+            transferring to human
+            
+            Response: True|
+                
+            You *must* always return your response in this format
+                Example for terminal is True:
+                True|
+                Example for terminal is False:
+                False|Yes, I'm an existing customer;No, I'm not an existing customer
+                
+            There are two splitters so you cannot use them anywhere else: | and ;
             """
         ).strip()
